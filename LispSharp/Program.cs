5db7d6819b91b25adc303255b0c3ade924a8b0ff
Brian Bramsted <bbramsted@geddion.com>
2016-06-15 22:42:03 +0400

    Initial commit

---
+++ LispSharp/Program.cs
@@ -0,0 +1,414 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LispSharp
+{
+
+    public class Cell
+    {
+        public enum CellType { Undefined, Number, Symbol, Bool, List, Procedure, Function}
+
+        public CellType Type;
+        public float number;
+        public bool boolval;
+        public string symbol = null;
+        public List<Cell> list = new List<Cell>();
+        public Procedure proc = null;
+        public Delegate func = null;
+
+        public override string ToString()
+        {
+            if (Type == CellType.Number)
+                return number.ToString() + " ";
+            else if (Type == CellType.Symbol)
+                return symbol.ToString() + " ";
+            else if (Type == CellType.Bool)
+                return boolval.ToString() + " ";
+            else if (Type == CellType.List)
+            {
+                string res = "(";
+                foreach (var c in list)
+                    res += c.ToString();
+                res = res.Trim() + ") ";
+                return res;
+            }
+            else return "";
+        }
+    }
+
+    public class Env
+    {
+        Env outer = null;
+        Dictionary<string, Cell> data = new Dictionary<string, Cell>();
+
+        public Env()
+        {
+        }
+
+        public Env(List<string> parms, List<Cell> args, Env outer = null)
+        {
+            this.outer = outer;
+            for (int i = 0; i < parms.Count; i++)
+                data.Add(parms[i], args[i]);
+        }
+
+        public Cell this[string key]
+        {
+            get
+            {
+                Cell res;
+
+                if (data.TryGetValue(key, out res))
+                    return res;
+                else if (outer != null)
+                    return outer[key];
+                else
+                    throw new ArgumentOutOfRangeException("Undefined symbol " + key);
+            }
+            set
+            {
+                data[key] = value;
+            }
+        }
+    }
+
+    public class Procedure
+    {
+        List<String> parms;
+        Cell body;
+        Env env;
+
+        public Procedure(Cell parms, Cell body, Env env)
+        {
+            this.parms = parms.list.Select(x => x.symbol).ToList();
+            this.body = body;
+            this.env = env;
+        }
+
+        public Cell Call(List<Cell> args)
+        {
+            Env localEnv = new Env(parms, args, env);
+            return Program.Eval(body, localEnv);
+        }
+    }
+
+
+    public class Program
+    {
+        static Env GlobalEnv = new Env();
+
+        //
+        // Initialize an environment with some standard functions
+        //
+        static Program()
+        {
+            GlobalEnv["+"] = new Cell() {
+                func = new Func<List<Cell>, Cell>(
+                x => new Cell()
+                {
+                    number = x[0].number + x[1].number,
+                    Type = Cell.CellType.Number
+                }),
+                Type = Cell.CellType.Function
+            };
+
+            GlobalEnv["-"] = new Cell()
+            {
+                func = new Func<List<Cell>, Cell>(
+                x => new Cell()
+                {
+                    number = x[0].number - x[1].number,
+                    Type = Cell.CellType.Number
+                }),
+                Type = Cell.CellType.Function
+            };
+
+            GlobalEnv["*"] = new Cell()
+            {
+                func = new Func<List<Cell>, Cell>(
+                x => new Cell()
+                {
+                    number = x[0].number * x[1].number,
+                    Type = Cell.CellType.Number
+                }),
+                Type = Cell.CellType.Function
+            };
+
+            GlobalEnv["/"] = new Cell()
+            {
+                func = new Func<List<Cell>, Cell>(
+                x => new Cell()
+                {
+                    number = x[0].number / x[1].number,
+                    Type = Cell.CellType.Number
+                }),
+                Type = Cell.CellType.Function
+            };
+
+            GlobalEnv["="] = new Cell()
+            {
+                func = new Func<List<Cell>, Cell>(
+                x => new Cell()
+                {
+                    boolval = x[0].number == x[1].number,
+                    Type = Cell.CellType.Bool
+                }),
+                Type = Cell.CellType.Function
+            };
+
+            GlobalEnv["<"] = new Cell()
+            {
+                func = new Func<List<Cell>, Cell>(
+                x => new Cell()
+                {
+                    boolval = x[0].number < x[1].number,
+                    Type = Cell.CellType.Bool
+                }),
+                Type = Cell.CellType.Function
+            };
+
+            GlobalEnv[">"] = new Cell()
+            {
+                func = new Func<List<Cell>, Cell>(
+                x => new Cell()
+                {
+                    boolval = x[0].number > x[1].number,
+                    Type = Cell.CellType.Bool
+                }),
+                Type = Cell.CellType.Function
+            };
+
+            GlobalEnv["<="] = new Cell()
+            {
+                func = new Func<List<Cell>, Cell>(
+                x => new Cell()
+                {
+                    boolval = x[0].number <= x[1].number,
+                    Type = Cell.CellType.Bool
+                }),
+                Type = Cell.CellType.Function
+            };
+
+            GlobalEnv[">="] = new Cell()
+            {
+                func = new Func<List<Cell>, Cell>(
+                x => new Cell()
+                {
+                    boolval = x[0].number >= x[1].number,
+                    Type = Cell.CellType.Bool
+                }),
+                Type = Cell.CellType.Function
+            };
+
+            GlobalEnv["car"] = new Cell()
+            {
+                func = new Func<List<Cell>, Cell>(x => x[0].list.First()),
+                Type = Cell.CellType.Function
+            };
+
+            GlobalEnv["cdr"] = new Cell()
+            {
+                func = new Func<List<Cell>, Cell>(
+                x => new Cell()
+                {
+                    list = x[0].list.Skip(1).ToList(),
+                    Type = Cell.CellType.List
+                }),
+                Type = Cell.CellType.Function
+            };
+
+            GlobalEnv["cons"] = new Cell()
+            {
+                func = new Func<List<Cell>, Cell>(
+                x => 
+                {
+                    Cell res = new Cell() { Type = Cell.CellType.List };
+                    res.list.Add(x[0]);
+                    res.list.AddRange(x[1].list);
+                    return res;
+                }),
+                Type = Cell.CellType.Function
+            };
+
+            GlobalEnv["append"] = new Cell()
+            {
+                func = new Func<List<Cell>, Cell>(
+                x =>
+                {
+                    Cell res = new Cell() { Type = Cell.CellType.List };
+                    res.list.AddRange(x[0].list);
+                    res.list.AddRange(x[1].list);
+                    return res;
+                }),
+                Type = Cell.CellType.Function
+            };
+
+            GlobalEnv["length"] = new Cell()
+            {
+                func = new Func<List<Cell>, Cell>(
+                x => new Cell()
+                {
+                    number = x[0].list.Count,
+                    Type = Cell.CellType.Number
+                }),
+                Type = Cell.CellType.Function
+            };
+
+            GlobalEnv["null?"] = new Cell()
+            {
+                func = new Func<List<Cell>, Cell>(
+                x => new Cell()
+                {
+                    boolval = x[0].list.Count == 0,
+                    Type = Cell.CellType.Bool
+                }),
+                Type = Cell.CellType.Function
+            };
+
+            GlobalEnv["list"] = new Cell()
+            {
+                func = new Func<List<Cell>, Cell>(
+                x => new Cell()
+                {
+                    list = x,
+                    Type = Cell.CellType.List
+                }),
+                Type = Cell.CellType.Function
+            };
+
+        }
+
+        //
+        // Eval
+        //
+        static public Cell Eval(Cell x)
+        {
+            return Eval(x, GlobalEnv);
+        }
+
+        static public Cell Eval(Cell x, Env env)
+        {
+            if (x.Type == Cell.CellType.Undefined)
+                throw new Exception("Undefined cell type");
+
+            // Symbol
+            if (x.Type == Cell.CellType.Symbol)
+                return env[x.symbol];
+
+            // Number
+            else if (x.Type != Cell.CellType.List)
+                return x;
+            
+            // List           
+            if (x.list[0].symbol == "begin")
+            {
+                Cell res = null;
+                foreach (var exp in x.list.Skip(1))
+                    res = Eval(exp, env);
+                return res;
+            }
+            else if (x.list[0].symbol == "quote")
+            {
+                return x.list[1];
+            }
+            else if (x.list[0].symbol == "if")
+            {
+                var test = x.list[1];
+                var conseq = x.list[2];
+                var alt = x.list[3];
+                var exp = (Eval(test, env).boolval) == true ? conseq : alt;
+                return Eval(exp, env);
+            }
+            else if (x.list[0].symbol == "define")
+            {
+                var variable = x.list[1];
+                var exp = x.list[2];
+                env[variable.symbol] = Eval(exp, env);
+                return null;
+            }
+            else if (x.list[0].symbol == "set!")
+            {
+                env[x.list[1].symbol] = Eval(x.list[2], env);
+                return null;
+            }
+            else if (x.list[0].symbol == "lambda")
+            {
+                return new Cell() { proc = new Procedure(x.list[1], x.list[2], env), Type = Cell.CellType.Procedure };
+            }
+            else {
+                var proc = Eval(x.list[0], env);
+                var args = x.list.Skip(1).Select(n => Eval(n, env)).ToList();
+
+                if (proc.Type == Cell.CellType.Procedure)
+                {
+                    // Lambda call
+                    return proc.proc.Call(args);
+                }
+                else {
+                    // Function call
+                    return (Cell) proc.func.DynamicInvoke(args);
+                }
+            }
+        }
+
+        //
+        // Parsing
+        //
+        public static Cell Parse(string program)
+        {
+            return ReadFromTokens(Tokenize(program));
+        }
+
+        private static Stack<string> Tokenize(string program)
+        {
+            IEnumerable<string> tokens = program.Replace(")", " ) ").Replace("(", " ( ").Split(' ').Select(x => x.Trim()).Where(x => x != "");
+            return new Stack<string>(tokens.Reverse());
+        }
+
+        private static Cell ReadFromTokens(Stack<string> tokens)
+        {
+            if (tokens.Count == 0)
+                throw new Exception("Syntax error: Unexpected EOF");
+
+            string token = tokens.Pop();
+            if ("(" == token)
+            {
+                var L = new Cell() { Type = Cell.CellType.List };
+                while (tokens.Peek() != ")")
+                    L.list.Add(ReadFromTokens(tokens));
+
+                tokens.Pop(); // Remmove the ")"
+                return L;
+            }
+            else if (")" == token)
+            {
+                throw new Exception("Syntax error: Unexpected )");
+            }
+            else
+            {
+                float f;
+                if (float.TryParse(token, out f))
+                    return new Cell() { number = f, Type = Cell.CellType.Number };
+                else
+                    return new Cell() { symbol = token, Type = Cell.CellType.Symbol };
+            }
+        }
+
+        // 
+        // Main REPL
+        //
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Lisp>");
+                string prog = Console.ReadLine();
+                var res = Eval(Parse(prog), GlobalEnv);
+                if (res != null)
+                    Console.WriteLine(res.ToString());
+            }
+        }
+
+    }
+}
---
+++ ListSharpTest/UnitTest.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LispSharp;
+
+namespace ListSharpTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void Basic()
+        {
+            Cell res = Program.Eval(Program.Parse("(quote (testing 1 (2.0) -3.14e9))"));
+            Assert.AreEqual(res.Type, Cell.CellType.List);
+            Assert.AreEqual(res.list.Count, 4);
+            Assert.AreEqual(res.list[0].symbol, "testing");
+            Assert.AreEqual(res.list[1].number, 1f);
+            Assert.AreEqual(res.list[2].list[0].number, 2f);
+            Assert.AreEqual(res.list[3].number, -3.14e9f);
+
+            res = Program.Eval(Program.Parse("(+ 2 2)"));
+            Assert.AreEqual(res.Type, Cell.CellType.Number);
+            Assert.AreEqual(res.number, 4f);
+
+            res = Program.Eval(Program.Parse("(+ (* 2 100) (* 1 10))"));
+            Assert.AreEqual(res.Type, Cell.CellType.Number);
+            Assert.AreEqual(res.number, 210f);
+
+            res = Program.Eval(Program.Parse("(if (> 6 5) (+ 1 1) (+ 2 2))"));
+            Assert.AreEqual(res.Type, Cell.CellType.Number);
+            Assert.AreEqual(res.number, 2f);
+
+            res = Program.Eval(Program.Parse("(if (< 6 5) (+ 1 1) (+ 2 2))"));
+            Assert.AreEqual(res.Type, Cell.CellType.Number);
+            Assert.AreEqual(res.number, 4f);
+        }
+
+        [TestMethod]
+        public void Env()
+        {
+
+            Cell res = Program.Eval(Program.Parse("(define x 3)"));
+            Assert.AreEqual(res, null);
+
+            res = Program.Eval(Program.Parse("x"));
+            Assert.AreEqual(res.Type, Cell.CellType.Number);
+            Assert.AreEqual(res.number, 3f);
+
+            res = Program.Eval(Program.Parse("(+ x x)"));
+            Assert.AreEqual(res.Type, Cell.CellType.Number);
+            Assert.AreEqual(res.number, 6f);
+
+            res = Program.Eval(Program.Parse("(begin (define x 1) (set! x (+ x 1)) (+ x 1))"));
+            Assert.AreEqual(res.Type, Cell.CellType.Number);
+            Assert.AreEqual(res.number, 3f);
+        }
+
+        [TestMethod]
+        public void Lambda()
+        {
+
+            Cell res = Program.Eval(Program.Parse("((lambda (x) (+ x x)) 5)"));
+            Assert.AreEqual(res.Type, Cell.CellType.Number);
+            Assert.AreEqual(res.number, 10f);
+
+            res = Program.Eval(Program.Parse("(define twice (lambda (x) (* 2 x)))"));
+            Assert.AreEqual(res, null);
+
+            res = Program.Eval(Program.Parse("(twice 5)"));
+            Assert.AreEqual(res.Type, Cell.CellType.Number);
+            Assert.AreEqual(res.number, 10f);
+
+            res = Program.Eval(Program.Parse("(define compose (lambda (f g) (lambda (x) (f (g x)))))"));
+            Assert.AreEqual(res, null);
+
+            res = Program.Eval(Program.Parse("((compose list twice) 5)"));
+            Assert.AreEqual(res.Type, Cell.CellType.List);
+            Assert.AreEqual(res.list.Count, 1);
+            Assert.AreEqual(res.list[0].number, 10f);
+
+            res = Program.Eval(Program.Parse("(define repeat (lambda (f) (compose f f)))"));
+            Assert.AreEqual(res, null);
+
+            res = Program.Eval(Program.Parse("((repeat twice) 5)"));
+            Assert.AreEqual(res.Type, Cell.CellType.Number);
+            Assert.AreEqual(res.number, 20f);
+
+            res = Program.Eval(Program.Parse("((repeat (repeat twice)) 5)"));
+            Assert.AreEqual(res.Type, Cell.CellType.Number);
+            Assert.AreEqual(res.number, 80f);
+        }
+
+        [TestMethod]
+        public void Recursion()
+        {
+            Cell res = Program.Eval(Program.Parse("(define fact (lambda (n) (if (<= n 1) 1 (* n (fact (- n 1))))))"));
+            Assert.AreEqual(res, null);
+
+            res = Program.Eval(Program.Parse("(fact 3)"));
+            Assert.AreEqual(res.Type, Cell.CellType.Number);
+            Assert.AreEqual(res.number, 6f);
+
+            res = Program.Eval(Program.Parse("(fact 10)"));
+            Assert.AreEqual(res.Type, Cell.CellType.Number);
+            Assert.AreEqual(res.number, 3628800f);
+        }
+
+        [TestMethod]
+        public void List()
+        {
+            Cell res = Program.Eval(Program.Parse("(define abs (lambda (n) ((if (> n 0) + -) 0 n)))"));
+            Assert.AreEqual(res, null);
+
+            res = Program.Eval(Program.Parse("(list (abs -3) (abs 0) (abs 3))"));
+            Assert.AreEqual(res.Type, Cell.CellType.List);
+            Assert.AreEqual(res.list.Count, 3);
+            Assert.AreEqual(res.list[0].number, 3f);
+            Assert.AreEqual(res.list[1].number, 0f);
+            Assert.AreEqual(res.list[2].number, 3f);
+
+            res = Program.Eval(Program.Parse(@"(define combine (lambda (f)
+                                                (lambda (x y)
+                                                    (if (null? x) (quote ())
+                                                        (f (list (car x) (car y))
+                                                            ((combine f) (cdr x) (cdr y)))))))"));
+            Assert.AreEqual(res, null);
+
+            res = Program.Eval(Program.Parse("(define zip (combine cons))"));
+            Assert.AreEqual(res, null);
+
+            res = Program.Eval(Program.Parse("(zip (list 1 2 3 4) (list 5 6 7 8))"));
+            Assert.AreEqual(res.Type, Cell.CellType.List);
+            Assert.AreEqual(res.list.Count, 4);
+            Assert.AreEqual(res.list[0].list[0].number, 1f);
+            Assert.AreEqual(res.list[0].list[1].number, 5f);
+            Assert.AreEqual(res.list[1].list[0].number, 2f);
+            Assert.AreEqual(res.list[1].list[1].number, 6f);
+            Assert.AreEqual(res.list[2].list[0].number, 3f);
+            Assert.AreEqual(res.list[2].list[1].number, 7f);
+            Assert.AreEqual(res.list[3].list[0].number, 4f);
+            Assert.AreEqual(res.list[3].list[1].number, 8f);
+
+            res = Program.Eval(Program.Parse(@"
+                (define riff-shuffle(lambda(deck)(begin
+                    (define take(lambda(n seq)(if (<= n 0) (quote())(cons(car seq)(take(- n 1)(cdr seq))))))
+                        (define drop(lambda(n seq)(if (<= n 0) seq(drop(- n 1)(cdr seq)))))
+                            (define mid(lambda(seq)(/ (length seq) 2)))
+                                ((combine append) (take(mid deck) deck) (drop(mid deck) deck)))))"));
+            Assert.AreEqual(res, null);
+
+            res = Program.Eval(Program.Parse("(riff-shuffle (list 1 2 3 4 5 6 7 8))"));
+            Assert.AreEqual(res.Type, Cell.CellType.List);
+            Assert.AreEqual(res.list.Count, 8);
+            Assert.AreEqual(res.list[0].number, 1f);
+            Assert.AreEqual(res.list[1].number, 5f);
+            Assert.AreEqual(res.list[2].number, 2f);
+            Assert.AreEqual(res.list[3].number, 6f);
+            Assert.AreEqual(res.list[4].number, 3f);
+            Assert.AreEqual(res.list[5].number, 7f);
+            Assert.AreEqual(res.list[6].number, 4f);
+            Assert.AreEqual(res.list[7].number, 8f);
+
+            res = Program.Eval(Program.Parse("(define compose (lambda (f g) (lambda (x) (f (g x)))))"));
+            Assert.AreEqual(res, null);
+
+            res = Program.Eval(Program.Parse("(define repeat (lambda (f) (compose f f)))"));
+            Assert.AreEqual(res, null);
+
+            res = Program.Eval(Program.Parse("((repeat riff-shuffle) (list 1 2 3 4 5 6 7 8))"));
+            Assert.AreEqual(res.Type, Cell.CellType.List);
+            Assert.AreEqual(res.list.Count, 8);
+            Assert.AreEqual(res.list[0].number, 1f);
+            Assert.AreEqual(res.list[1].number, 3f);
+            Assert.AreEqual(res.list[2].number, 5f);
+            Assert.AreEqual(res.list[3].number, 7f);
+            Assert.AreEqual(res.list[4].number, 2f);
+            Assert.AreEqual(res.list[5].number, 4f);
+            Assert.AreEqual(res.list[6].number, 6f);
+            Assert.AreEqual(res.list[7].number, 8f);
+
+            res = Program.Eval(Program.Parse("(riff-shuffle (riff-shuffle (riff-shuffle (list 1 2 3 4 5 6 7 8))))"));
+            Assert.AreEqual(res.Type, Cell.CellType.List);
+            Assert.AreEqual(res.list.Count, 8);
+            Assert.AreEqual(res.list[0].number, 1f);
+            Assert.AreEqual(res.list[1].number, 2f);
+            Assert.AreEqual(res.list[2].number, 3f);
+            Assert.AreEqual(res.list[3].number, 4f);
+            Assert.AreEqual(res.list[4].number, 5f);
+            Assert.AreEqual(res.list[5].number, 6f);
+            Assert.AreEqual(res.list[6].number, 7f);
+            Assert.AreEqual(res.list[7].number, 8f);
+        }
+        
+    }
+}
